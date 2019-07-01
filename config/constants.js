2c8b3e7cf4c938c105676bf761d7d8dbc18d8ff8
shana <Natalia.S@kozak-group.com>
2019-07-01 15:03:44 +0300

    add server models

--- config/constants.js
+++ config/constants.js
@@ -3,9 +3,15 @@ module.exports = {
     user: 'app.user',
     team: 'team',
     project: 'project',
+    userProject: 'userProject',
+    userTeam: 'userTeam',
   },
   server_models: {
-    'app.user': 'mozzler\\auth\\models\\User',
+    'app.user': 'app\\models\\User',
+    'project': 'app\\models\\Project',
+    'userProject': 'app\\models\\UserProject',
+    'userTeam': 'app\\models\\UserTeam',
+    'team': 'app\\models\\Team'
   },
   routes: {
     user: 'user',
