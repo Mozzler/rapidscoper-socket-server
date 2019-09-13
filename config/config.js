f3dc87758d415a6ab2e966f8e2ba42fb37053753
Chris Were <chris@mozzler.com.au>
2019-09-13 14:58:52 +0930

    Update port

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     HOST: 'localhost',
-    PORT: 8888,
+    PORT: 8081,
     REDIS_PORT: 6379,
     SOCKETS_PORT: 3000,
     MONGO_URI: 'mongodb://rs-1:986f7ga8906a07d@ds031003-a0.mongolab.com:31003,ds031003-a1.mongolab.com:31003/rs-1?replicaSet=rs-ds031003',
